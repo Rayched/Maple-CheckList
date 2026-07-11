0b6d422d26b0453d2a8761327b2b00a099edf2ed
Rayched <ny2401@naver.com>
2026-07-11 23:15:26 +0900

    20260711 'f343e58' error 해결..?

--- src/utils/SortFuncs.ts
+++ src/utils/SortFuncs.ts
@@ -1,57 +1,9 @@
 
 import { I_BossIncomeData } from "@/app/incomes/add_incomes/_components/AddIncomeForms";
-import { I_BossToDos, I_WeeklyToDos } from "@/stores/CharToDoStore";
-
-interface I_BossToDoSortProps {
-    BossToDoDatas: I_BossToDos[];
-};
-
-interface I_WeeklyToDoSortProps {
-    WeeklyToDoDatas: I_WeeklyToDos[];
-};
 
 interface I_IncomeDataSortProps {
     IncomeDatas: I_BossIncomeData[];
 }; 
-
-export function BossToDoSort({BossToDoDatas}: I_BossToDoSortProps){
-    const ToDoSortOutput = BossToDoDatas.sort((a, b) => {
-        if(String(a.contentsId) > String(b.contentsId)){
-            return 1;
-        } else if(String(a.contentsId) < String(b.contentsId)){
-            return -1;
-        } else {
-            return 0;
-        }
-    });
-
-    return ToDoSortOutput;
-};
-
-export function WeeklyToDoSort({WeeklyToDoDatas}: I_WeeklyToDoSortProps){
-    const WeeklySorts = (Contents: I_WeeklyToDos[]) => {
-        const Outputs = Contents.sort((a, b) => {
-            if(String(a.contentsId) > String(b.contentsId)){
-                return 1;
-            } else if(String(a.contentsId) < String(b.contentsId)){
-                return -1;
-            } else {
-                return 0;
-            }
-        })
-
-        return Outputs;
-    };
-    
-    const AccountFilter = WeeklySorts(
-        WeeklyToDoDatas.filter((data) => data.contentsUnit === "account")
-    );
-    const CharFilter = WeeklySorts(
-        WeeklyToDoDatas.filter((data) => data.contentsUnit !== "account")
-    );
-
-    return [...AccountFilter, ...CharFilter];
-}
 
 /**
  * '/incomes' page, data sort function
